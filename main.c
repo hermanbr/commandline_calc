6ae4955917b8cf67f41618d0da66257e02d2e8d3
hermabj <hermabj@ifi.uio.no>
2025-05-21 12:46:27 +0200

    Added header file. MAde proper tokenizer so it now works with double digits

--- main.c
+++ main.c
@@ -5,45 +5,22 @@
 #include <math.h>
 
 
+#include "main.h"
+
 
 
 
 /** TODO
+ * Change tokeizer so it works with multi digit numbers
  * Implement a --help
  * Choice between int and float
  * How to see if legal input 
  * 
  */
 
-#define ADD '+'
-#define SUB '-'
-#define POW '^'
-#define MUL '*'
-#define DIV '/'
-#define LEFT_BRACKET '('
-#define RIGHT_BRACKET ')'
-
-#define LEFT_ASSOCIATIVE  0
-#define RIGHT_ASSOCIATIVE  1
-
-#define ADD_ASSOCIATIVE LEFT_ASSOCIATIVE
-#define MUL_ASSOCIATIVE LEFT_ASSOCIATIVE
-
-#define ADD_PRECEDENDCE 0
-#define SUB_PRECEDENCE 0
-#define MUL_PRECEDENCE 1
-#define DIV_PRECEDENCE 1
-#define POW_PRECEDENCE 2
-#define BRACKET_PRECEDENCE 3
 
 
-
-
-
-
-
- static int use_operator(int left_value, int right_value, char operator);
-
+struct type *tokenized_list;
  
  
 
@@ -71,6 +48,13 @@ static int is_operator(char op){
     || op == SUB || op == POW || op == MUL || op == DIV);
 }
 
+static int is_left_bracket(char op){
+    return (op == LEFT_BRACKET);
+}
+
+static int is_right_bracket(char op){
+    return (op == RIGHT_BRACKET);
+}
 
 void print_output(int index, char *output){
     for(int i = 0; i < index; i++){
@@ -79,107 +63,30 @@ void print_output(int index, char *output){
 }
 
 int main(int argc, char **argv){
-    int result;
-    char *token;
-    char output[100];
-    char operator_stack[100];
-    int stack_index = 0;
-    int output_index = 0;
-    
+    // int result;
+    // char *token;
+    // char output[100];
+    // char operator_stack[100];
+    // int stack_index = 0;
+    // int output_index = 0;
+    int num_tokens = 0;
+    int tokens_rpn; // Brackets are removed in RPN
 
     if (argc < 1){
         printf("No arguments\n");
         return -1;
     }
 
-    /*
-           {
-            If ((token = left associative AND 
-                 precedence <= stack top element) OR
-            (token = right associative AND 
-                 precedence < stack top element))
-            {
-            Pop stack onto the output queue.  
-                Exit while loop.
-            }
-        }*/
+    tokenizer(argv[1], &num_tokens);
+    struct type rpn_list[num_tokens]; 
 
-    token = argv[1];
-    int length = strlen(argv[1]);
-    char *stack_top = operator_stack;
-    // while(*token != '/'){
-    for(int i = 0; i < length; i++){
-        printf("stack top: %c\n", *stack_top);
-        if(isdigit((int)*token)){
-            output[output_index++] = *token;
-        }
-        else if(is_operator(*token)){
-            while((stack_index > 0) && is_operator(*stack_top)){
-                if((get_association(*token) == LEFT_ASSOCIATIVE && (get_priority(*token) <= get_priority(*stack_top))) \
-                    || ((get_association(*token)==RIGHT_ASSOCIATIVE) &&  get_priority(*token) < get_priority(*stack_top))
-                ){
-                    printf("get here \n");
-                    output[output_index++] = operator_stack[--stack_index];
-                    stack_top = &operator_stack[stack_index-1];
-                    continue;
-                }
-                break;
-            }
-            
-            operator_stack[stack_index++] = *token;
-            stack_top = &operator_stack[stack_index-1];
-        }
+    make_RPN(rpn_list, num_tokens, &tokens_rpn);
 
-        token++; // Onto the next
-    }
-    // Pop stack onto queue
-    while(stack_index > 0){
-        stack_index--;
-        output[output_index++] = operator_stack[stack_index];
-    }
 
-    for(int i = 0; i < output_index; i++){
-        printf("output queue %c\n", output[i]);
-    }
+    free(tokenized_list);
+    tokenized_list = NULL;
 
-    // Now evaluate expression in Reverse Polish Notation
-   
-
-     for(int i = 0; i < output_index; i++){
-        char tok = output[i];
-        printf("output queue 2 %c\n", tok);
-    }
-
-    int output_stack[100];
-    int output_stack_index = 0;
-    for(int i = 0; i < output_index; i++){
-
-        char tok = output[i];
-        printf("token = %c\n", tok);
-        if(isdigit((unsigned char) tok)){ // Then push on output stack
-            output_stack[output_stack_index] = tok - '0';
-            output_stack_index++;
-        }
-        else if ((is_operator(tok))){ // If operand pop 2 of stack and evaluate
-            int first, second, calc;
-            output_stack_index--;
-            second = output_stack[output_stack_index];
-            output_stack_index--;
-            first = output_stack[output_stack_index];
-
-            calc = use_operator(first, second, output[i]);
-            printf("calc %d\n", calc);
-
-            output_stack[output_stack_index] = calc;
-            output_stack_index++;
-        }
-
-    }
-
-    // At this point there should only be one value on the output stack. 
-    printf("Sum = %d\n", output_stack[output_stack_index]);
-    return 1;
-
+    calculate_RPN(rpn_list, tokens_rpn);
 
 }
 
@@ -197,3 +104,157 @@ static int use_operator(int left_value, int right_value, char operator){
     }
     return -1;
 }
+
+/**
+ * TODO
+ * Error checking
+ */
+static void tokenizer(char *argv, int *num_tokens){
+    struct type tokenized_intermediate_list[strlen(argv)];
+
+    char *str = argv;
+    char *end;
+    int base = 10;
+    size_t n = 0;
+    // Go through the string, tokenize using 
+    while(*str){
+        // If its a digit it may be using several digits, so we use strtol() to extract this. 
+        if(isdigit((unsigned char)*str)){
+            long value = strtol(str, &end, base);
+            struct type token;
+            token.type = DIGIT;
+            token.val = (int)value;
+            tokenized_intermediate_list[n++] = token;
+            str = end;
+        } 
+        else if(is_operator((unsigned char)*str)){
+            struct type token;
+            token.type = OPERAND;
+            token.caracter = (unsigned char)*str;
+            tokenized_intermediate_list[n++] = token;
+            str++;
+        }
+        else if(is_left_bracket((unsigned char)*str) || is_right_bracket((unsigned char)*str)){
+            struct type token;
+            token.type = BRACKET;
+            token.caracter = (unsigned char)*str;
+            tokenized_intermediate_list[n++] = token;
+            str++;
+        }
+    }
+
+    tokenized_list = malloc(sizeof(struct type) * n);
+    for(int i = 0; i < n; i++){
+        tokenized_list[i] = tokenized_intermediate_list[i];
+    }
+    *num_tokens = n;
+
+
+}
+
+static void make_RPN(struct type *output_list, int num_tokens, int *tokens_rpn){
+
+   
+    int output_index = 0;
+    struct type operator_stack[num_tokens];
+    int operator_stack_index = 0;
+    struct type *stack_top = operator_stack;
+
+
+        for(int i = 0; i < num_tokens;  i++){
+            struct type token = tokenized_list[i];
+
+            if(tokenized_list[i].type == DIGIT){
+                output_list[output_index++] = tokenized_list[i];
+            }
+
+            else if(token.type == OPERAND){
+                while(operator_stack_index > 0 && stack_top->type==OPERAND){
+                    if(((get_association((token.caracter)) == LEFT_ASSOCIATIVE) \
+                    && get_priority(token.caracter) <= get_priority(stack_top->caracter))\
+                        || ((get_association(token.caracter) == RIGHT_ASSOCIATIVE) \
+                        && get_priority(token.caracter) < get_priority(stack_top->caracter)))
+                        {
+                            output_list[output_index++] = operator_stack[--operator_stack_index];
+                            stack_top = &operator_stack[operator_stack_index-1];
+                            continue;
+                        }
+                        break;
+                }
+
+                operator_stack[operator_stack_index++] = token;
+                stack_top = &operator_stack[operator_stack_index-1];
+            }
+            else if(is_left_bracket(token.caracter)){
+                operator_stack[operator_stack_index++] = token;
+                stack_top = &operator_stack[operator_stack_index-1];
+
+            }
+            else if(is_right_bracket(token.caracter)){
+
+                while(output_index > 0 &&  !is_left_bracket(stack_top->caracter)){ // While not ' ( '
+                    output_list[output_index++] = operator_stack[--operator_stack_index];
+                    stack_top = &operator_stack[operator_stack_index-1];
+                } // Remove ' ( '
+                operator_stack_index--;
+                stack_top =  &operator_stack[operator_stack_index-1];
+            }
+            
+
+
+        }
+
+    //Pop stack onto queue
+    while(operator_stack_index > 0){
+        operator_stack_index--;
+        output_list[output_index++] = operator_stack[operator_stack_index];
+    }
+
+    *tokens_rpn = output_index;
+
+    for(int i = 0; i < output_index; i++){
+        if(output_list[i].type == DIGIT){
+            printf("output queue %d\n", output_list[i].val);
+        }
+        else{
+            printf("output queue %c\n", output_list[i].caracter);
+        }
+    }
+
+}
+
+static int calculate_RPN(struct type *list, int num_tokens){
+
+        // // Now evaluate expression in Reverse Polish Notation
+
+    // int output_stack[100];
+    struct type output_stack[num_tokens];
+    int output_stack_index = 0;
+
+    for(int i = 0; i < num_tokens; i++){
+        struct type token = list[i];
+
+        if(token.type == DIGIT){ // Push output on stack
+            output_stack[output_stack_index++] = token;
+        }
+        else if(token.type == OPERAND){ // If operand pop 2 of stack and evaluate
+            int first, second, calc;
+            second = output_stack[--output_stack_index].val;
+            first = output_stack[--output_stack_index].val;
+
+            calc = use_operator(first, second, token.caracter);
+            struct type calculated;
+            calculated.type = DIGIT;
+            calculated.val = calc;
+
+            output_stack[output_stack_index++] = calculated;
+        }
+
+    }
+
+    printf("Sum = %d\n", output_stack[0].val);
+    return 1;
+
+
+
+};
---
+++ main.h
@@ -0,0 +1,46 @@
+#define ADD '+'
+#define SUB '-'
+#define POW '^'
+#define MUL '*'
+#define DIV '/'
+#define LEFT_BRACKET '('
+#define RIGHT_BRACKET ')'
+
+#define LEFT_ASSOCIATIVE  0
+#define RIGHT_ASSOCIATIVE  1
+
+#define ADD_ASSOCIATIVE LEFT_ASSOCIATIVE
+#define MUL_ASSOCIATIVE LEFT_ASSOCIATIVE
+
+#define ADD_PRECEDENDCE 0
+#define SUB_PRECEDENCE 0
+#define MUL_PRECEDENCE 1
+#define DIV_PRECEDENCE 1
+#define POW_PRECEDENCE 2
+#define BRACKET_PRECEDENCE 3
+
+enum {
+    DIGIT,
+    OPERAND,
+    BRACKET
+};
+
+struct type {
+    char type;   // ADD, SUB etc.
+    int val;     // Actual value
+    char caracter; // Used if not digit
+};
+
+/* Methods used for setting up the Reverse Polish Notation*/
+
+static int use_operator(int left_value, int right_value, char op);
+static int get_priority(char op);
+static int get_association(char op);
+
+// Returns a list of 'struct types', also takes a parameter size to return how long it is.
+static void tokenizer(char *argv, int *num_tokens);
+
+// Take the tokenized list and return it as a REVERSEN POLISH NOTATION list
+static void make_RPN(struct type *list, int num_tokens, int *tokens_rpn);
+
+static int calculate_RPN(struct type *list, int num_tokens);
